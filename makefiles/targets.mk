7a208e85cdc971936ae9a5858f96639a47952c36
Charlie Chiang <charlie_c_0129@outlook.com>
2023-03-22 11:24:27 +0800

    revert: attach tty when building in build container
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- makefiles/targets.mk
+++ makefiles/targets.mk
@@ -28,7 +28,7 @@ build: gen-dockerignore build-dirs
 ifeq (1, $(USE_BUILD_CONTAINER))
 	echo "# BUILD using build container: $(BUILD_IMAGE)"
 	docker run                               \
-	    -it                                  \
+	    -i                                   \
 	    --rm                                 \
 	    --network host                       \
 	    -u $$(id -u):$$(id -g)               \
